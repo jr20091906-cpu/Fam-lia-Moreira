d54a033db2f816452ec1dbc6418585789cdb181c
jr20091906-cpu <jr20091906@hotmail.com>
2026-07-07 05:23:17 -0300

    Refactor login function to use user object

--- login.js
+++ login.js
@@ -1,57 +1,67 @@
 function entrar(){
 
-    let usuario = document.getElementById("usuario").value;
-    let senha = document.getElementById("senha").value;
+    const usuario = document.getElementById("usuario").value;
+    const senha = document.getElementById("senha").value;
 
 
-    if(usuario === "laercio" && senha === "1234"){
+    const usuarios = {
 
-        localStorage.setItem("nome","Laércio");
-        localStorage.setItem("perfil","Administrador");
+        "laercio":{
+            senha:"1234",
+            nome:"Laércio",
+            perfil:"Administrador"
+        },
+
+        "taina":{
+            senha:"1234",
+            nome:"Tainá",
+            perfil:"Responsável"
+        },
+
+        "anaclara":{
+            senha:"1234",
+            nome:"Ana Clara",
+            perfil:"Filha"
+        },
+
+        "isadora":{
+            senha:"1234",
+            nome:"Isadora",
+            perfil:"Filha"
+        },
+
+        "isaac":{
+            senha:"1234",
+            nome:"Isaac",
+            perfil:"Filho"
+        }
+
+    };
+
+
+    if(usuarios[usuario] && usuarios[usuario].senha === senha){
+
+
+        localStorage.setItem(
+            "nome",
+            usuarios[usuario].nome
+        );
+
+
+        localStorage.setItem(
+            "perfil",
+            usuarios[usuario].perfil
+        );
+
 
         window.location.href = "dashboard.html";
 
-    }
 
-    else if(usuario === "taina" && senha === "1234"){
+    }else{
 
-        localStorage.setItem("nome","Tainá");
-        localStorage.setItem("perfil","Responsável");
-
-        window.location.href = "dashboard.html";
-
-    }
-
-    else if(usuario === "anaclara" && senha === "1234"){
-
-        localStorage.setItem("nome","Ana Clara");
-        localStorage.setItem("perfil","Filha");
-
-        window.location.href = "dashboard.html";
-
-    }
-
-    else if(usuario === "isadora" && senha === "1234"){
-
-        localStorage.setItem("nome","Isadora");
-        localStorage.setItem("perfil","Filha");
-
-        window.location.href = "dashboard.html";
-
-    }
-
-    else if(usuario === "isaac" && senha === "1234"){
-
-        localStorage.setItem("nome","Isaac");
-        localStorage.setItem("perfil","Filho");
-
-        window.location.href = "dashboard.html";
-
-    }
-
-    else{
 
         alert("Usuário ou senha incorretos");
+
 
     }
 
