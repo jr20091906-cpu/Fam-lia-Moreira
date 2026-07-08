b338b26e404a52bbd7be0c587929d1b0988b68f5
jr20091906-cpu <jr20091906@hotmail.com>
2026-07-07 23:35:10 -0300

    Refactor login function to use member objects

--- login.js
+++ login.js
@@ -1,56 +1,86 @@
+const membros = {
+
+    laercio: {
+        nome: "Laércio",
+        senha: "201293",
+        perfil: "Administrador"
+    },
+
+    taina: {
+        nome: "Tainá",
+        senha: "120695",
+        perfil: "Responsável"
+    },
+
+    anaclara: {
+        nome: "Ana Clara",
+        senha: "050614",
+        perfil: "Filha"
+    },
+
+    isadora: {
+        nome: "Isadora",
+        senha: "010219",
+        perfil: "Filha"
+    },
+
+    isaac: {
+        nome: "Isaac",
+        senha: "010425",
+        perfil: "Filho"
+    }
+
+};
+
+
+let membroSelecionado = "";
+
+
+window.onload = function(){
+
+    const parametros = new URLSearchParams(window.location.search);
+
+    membroSelecionado = parametros.get("membro");
+
+
+    if(membroSelecionado && membros[membroSelecionado]){
+
+        document.getElementById("tituloMembro").innerHTML =
+        "👤 " + membros[membroSelecionado].nome;
+
+    }
+
+    else{
+
+        document.getElementById("tituloMembro").innerHTML =
+        "Membro não identificado";
+
+    }
+
+};
+
+
+
 function entrar(){
 
-    const usuario = document.getElementById("usuario").value;
-    const senha = document.getElementById("senha").value;
+    const senhaDigitada = document.getElementById("senha").value;
 
 
-    const usuarios = {
-
-        "laercio":{
-            senha:"1234",
-            nome:"Laércio",
-            perfil:"Administrador"
-        },
-
-        "taina":{
-            senha:"1234",
-            nome:"Tainá",
-            perfil:"Responsável"
-        },
-
-        "anaclara":{
-            senha:"1234",
-            nome:"Ana Clara",
-            perfil:"Filha"
-        },
-
-        "isadora":{
-            senha:"1234",
-            nome:"Isadora",
-            perfil:"Filha"
-        },
-
-        "isaac":{
-            senha:"1234",
-            nome:"Isaac",
-            perfil:"Filho"
-        }
-
-    };
-
-
-    if(usuarios[usuario] && usuarios[usuario].senha === senha){
-
+    if(
+        membroSelecionado &&
+        membros[membroSelecionado] &&
+        senhaDigitada === membros[membroSelecionado].senha
+    ){
 
         localStorage.setItem(
             "nome",
-            usuarios[usuario].nome
+            membros[membroSelecionado].nome
         );
 
 
         localStorage.setItem(
             "perfil",
-            usuarios[usuario].perfil
+            membros[membroSelecionado].perfil
         );
 
 
@@ -60,7 +90,7 @@ function entrar(){
     }else{
 
 
-        alert("Usuário ou senha incorretos");
+        alert("Senha incorreta");
 
 
     }
